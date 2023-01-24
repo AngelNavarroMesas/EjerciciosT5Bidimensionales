093fa34eec90fe895c4c22eab2a9ef9456fb7fb8
anavarro <anavarro@106-30.info2>
2023-01-24 11:12:18 +0100

    comiti

--- EjerciciosT5Bidimensinales/src/ejercicios/Ejercicio3.java
+++ EjerciciosT5Bidimensinales/src/ejercicios/Ejercicio3.java
@@ -1,10 +1,43 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int tabla[][] = new int[4][5];
+		int x=0, max=0, min=10, media=1;
 		
+		while(x<4) {
+			for(int i=0;i<=4;i++) {
+				System.out.println("Introduzca las notas del Alumno"+(x+1));
+				tabla[x][i] = sc.nextInt();
+			}
+			x++;
+		}
+		System.out.println(Arrays.deepToString(tabla));
+		x=0;
+		
+		while(x<4) {
+			for(int i=0;i<=4;i++) {
+				if(tabla[x][i]>max) {
+					max=tabla[x][i];
+				}
+				if(tabla[x][i]<min) {
+					min=tabla[x][i];
+				}
+				media+=tabla[x][i];
+			}
+			media/=5;
+			System.out.println("Media Alumno"+(x+1)+": "+media);
+			x++;
+			media=1;
+		}
+		System.out.println("Nota minima: "+min);
+		System.out.println("Nota minima: "+max);
 		
 	}
 
